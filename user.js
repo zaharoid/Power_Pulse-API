3640587b1cf4f10f271fc6b72025d83b70899ba6
zaharoid <gololobov.zahar23@gmail.com>
2023-11-09 23:48:38 +0100

    the end

---
+++ user.js
@@ -0,0 +1,31 @@
+// const user = {
+//   name: "11",
+//   email: "1@mail.com",
+//   password: "******",
+
+//   stats: {
+//     // height - number; minimum 150(cm); required
+//     // currentWeight - number; minimum 35(kg); required
+//     // desiredWeight - number; minimum 35(kg); required
+//     // birthday - date; must be older than 18 years;  required
+//     // blood - number; allowed values 1, 2, 3, 4; required
+//     // sex - string; allowed values "male", "female"; required
+//     // levelActivity - number; allowed values 1, 2, 3, 4, 5; required
+//   },
+// };
+
+//   dairy: {
+//     id: _id
+//     owner: user._id,
+//     products: [{ id,
+//              product(product ID) - string; required
+//              date - string; format dd/mm/YYYY; required || date
+//              amount - number; minimum 1(g); required
+//              calories - number; minimum 1; required
+//  }, {
+// id
+// }, {
+//     id
+// }],
+//     exercises: [{ id }, { id }, { id }],
+//   },
