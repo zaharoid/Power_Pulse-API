c6b85b0167af8bb6d03080aad065cea1f5df08d1
zaharoid <gololobov.zahar23@gmail.com>
2023-11-10 18:32:55 +0100

    init Swagger

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import logger from "morgan";
 import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./swagger.json" assert { type: "json" };
 import "dotenv/config";
 
 const app = express();
@@ -10,6 +12,7 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // app.use("/api/contacts", contactsRouter); // шаблон, кто забыл
 
