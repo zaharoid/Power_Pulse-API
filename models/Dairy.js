a1853f5815cd026575cc855fe53a0ccecd51524b
Nikita <barbos.nik007@gmail.com>
2023-11-10 23:00:31 +0100

    Start working on DAIRY
    I'm waiting on user and answer from mentor about dairy structure

---
+++ models/Dairy.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+import { handleSaveError, runValidatorsAtUpdate } from './mongooseUtils';
+
+const Dairy = new mongoose.Schema({
+    owner: {
+        type: String, // ID владельца
+        required: [true, "Diary without owner? Something wrong is going on here. Fix this and come back:)"],
+        unique: true
+    }
+
+})
