1583155a1d2d8579b4d25c0cf2bf1c9745d2481b
Nikita <barbos.nik007@gmail.com>
2023-11-10 23:25:27 +0100

    Just leave lol, this would not work

---
+++ models/Day.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+import { handleSaveError, runValidatorsAtUpdate } from './mongooseUtils';
+
+const Day = new mongoose.Schema({
+    products: {
+        type
+    }
+})
+
+day{
+    data: data
+    ex:{
+        {id: 92475678234, reps: 1231},
+        {id: 92475678234, reps: 1231},
+        {id: 92475678234, reps: 1231},
+        {id: 92475678234, reps: 1231}
+    },
+    products:{
+        [id: 2873462387468234, weight: 100]
+    },
+    kcalConsumed: lalala,
+    kcalBurned: okeokeoke
+
+
+}
+
+rice = findOne( id: 2873462387468234 )
+allKcal = rice.weight * ( rice.kcal / 100 )
