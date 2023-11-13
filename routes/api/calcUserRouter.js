fb5e2a59cf6804675590095b021e66f7c3ce9248
OLEH FEDORCHUK <fedorchuk2018@gmail.com>
2023-11-13 21:11:12 +0000

    update

--- routes/api/calcUserRouter.js
+++ routes/api/calcUserRouter.js
@@ -9,7 +9,6 @@ import calcUserController from "../../controllers/calcUserController.js";
 const calcUserRouter = express.Router();
 // calcUserRouter.get('/', authenticate, calcUserController.schow);
 calcUserRouter.get('/', authenticate, calcUserController.calculateCalories);
-
 calcUserRouter.post('/', authenticate, isEmptyBody, calcUserController.add);
 calcUserRouter.put('/:calcId', authenticate, calcUserController.updateById);
 
